a7285ce51051f2779f0974044e3a90a4cc97592d
Kristin Ruben <kruben@gmail.com>
2016-08-18 18:45:13 -0400

    pruning the view and adding content

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'netivot#index'
 
-  resources :netivot, only: [:new, :create, :edit, :update, :destroy] do
-    resources :network, only: [:new, :create, :edit, :update, :destroy]
-  end
+  resources :netivot, only: [:new, :create, :edit, :update, :destroy]
+  resources :network, only: [:new, :create, :edit, :update, :destroy]
+  resources :about, only: [:new, :create, :edit, :update, :destroy]
+  resources :team, only: [:new, :create, :edit, :update, :destroy]
+  resources :join, only: [:new, :create, :edit, :update, :destroy]
+  resources :contact, only: [:new, :create, :edit, :update, :destroy]
+  resources :resources, only: [:new, :create, :edit, :update, :destroy]
+  resources :events, only: [:new, :create, :edit, :update, :destroy]
 end
