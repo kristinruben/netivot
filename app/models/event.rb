b6148ec92dd8c3cdab1c6f4b301438601bac4cbb
Kristin Ruben <kristinruben@Kristins-MacBook.local>
2016-12-27 15:51:38 -0500

    continuing to build models

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
 
   belongs_to :location
 
-  has_many :attendees, dependent: :destroy
+  # has_many :attendees, dependent: :destroy
 
   attr_accessor :name, :start_at_time, :end_at_time, :start_at_date, :end_at_date,
     :location_display_string, :description
